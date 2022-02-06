aad231a7b2b3ddc9d60a22d0da1880db33059204
integral0131 <integral190131@gmail.com>
2022-02-06 12:28:34 +0900

    customhats

---
+++ Helpers.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Collections;
+using UnhollowerBaseLib;
+using UnityEngine;
+using System.Linq;
+using TownOfHost;
+using HarmonyLib;
+using Hazel;
+
+namespace TheOtherRoles
+{
+
+    public enum MurderAttemptResult
+    {
+        PerformKill,
+        SuppressKill,
+        BlankKill
+    }
+    public static class Helpers
+    {
+
+        public static Sprite loadSpriteFromResources(string path, float pixelsPerUnit)
+        {
+            try
+            {
+                Texture2D texture = loadTextureFromResources(path);
+                return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f), pixelsPerUnit);
+            }
+            catch
+            {
+                System.Console.WriteLine("Error loading sprite from path: " + path);
+            }
+            return null;
+        }
+
+        public static Texture2D loadTextureFromResources(string path)
+        {
+            try
+            {
+                Texture2D texture = new Texture2D(2, 2, TextureFormat.ARGB32, true);
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                Stream stream = assembly.GetManifestResourceStream(path);
+                var byteTexture = new byte[stream.Length];
+                var read = stream.Read(byteTexture, 0, (int)stream.Length);
+                LoadImage(texture, byteTexture, false);
+                return texture;
+            }
+            catch
+            {
+                System.Console.WriteLine("Error loading texture from resources: " + path);
+            }
+            return null;
+        }
+
+        public static Texture2D loadTextureFromDisk(string path)
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    Texture2D texture = new Texture2D(2, 2, TextureFormat.ARGB32, true);
+                    byte[] byteTexture = File.ReadAllBytes(path);
+                    LoadImage(texture, byteTexture, false);
+                    return texture;
+                }
+            }
+            catch
+            {
+                System.Console.WriteLine("Error loading texture from disk: " + path);
+            }
+            return null;
+        }
+
+        internal delegate bool d_LoadImage(IntPtr tex, IntPtr data, bool markNonReadable);
+        internal static d_LoadImage iCall_LoadImage;
+        private static bool LoadImage(Texture2D tex, byte[] data, bool markNonReadable)
+        {
+            if (iCall_LoadImage == null)
+                iCall_LoadImage = IL2CPP.ResolveICall<d_LoadImage>("UnityEngine.ImageConversion::LoadImage");
+            var il2cppArray = (Il2CppStructArray<byte>)data;
+            return iCall_LoadImage.Invoke(tex.Pointer, il2cppArray.Pointer, markNonReadable);
+        }
+        public static void setDefaultLook(this PlayerControl target)
+        {
+            target.setLook(target.Data.PlayerName, target.Data.DefaultOutfit.ColorId, target.Data.DefaultOutfit.HatId, target.Data.DefaultOutfit.VisorId, target.Data.DefaultOutfit.SkinId, target.Data.DefaultOutfit.PetId);
+        }
+
+        public static void setLook(this PlayerControl target, String playerName, int colorId, string hatId, string visorId, string skinId, string petId)
+        {
+            target.RawSetColor(colorId);
+            target.RawSetVisor(visorId);
+            target.RawSetHat(hatId, colorId);
+
+            SkinData nextSkin = DestroyableSingleton<HatManager>.Instance.GetSkinById(skinId);
+            PlayerPhysics playerPhysics = target.MyPhysics;
+            AnimationClip clip = null;
+            var spriteAnim = playerPhysics.Skin.animator;
+            var currentPhysicsAnim = playerPhysics.Animator.GetCurrentAnimation();
+            if (currentPhysicsAnim == playerPhysics.RunAnim) clip = nextSkin.RunAnim;
+            else if (currentPhysicsAnim == playerPhysics.SpawnAnim) clip = nextSkin.SpawnAnim;
+            else if (currentPhysicsAnim == playerPhysics.EnterVentAnim) clip = nextSkin.EnterVentAnim;
+            else if (currentPhysicsAnim == playerPhysics.ExitVentAnim) clip = nextSkin.ExitVentAnim;
+            else if (currentPhysicsAnim == playerPhysics.IdleAnim) clip = nextSkin.IdleAnim;
+            else clip = nextSkin.IdleAnim;
+            float progress = playerPhysics.Animator.m_animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+            playerPhysics.Skin.skin = nextSkin;
+            spriteAnim.Play(clip, 1f);
+            spriteAnim.m_animator.Play("a", 0, progress % 1);
+            spriteAnim.m_animator.Update(0f);
+
+            if (target.CurrentPet) UnityEngine.Object.Destroy(target.CurrentPet.gameObject);
+            target.CurrentPet = UnityEngine.Object.Instantiate<PetBehaviour>(DestroyableSingleton<HatManager>.Instance.GetPetById(petId).PetPrefab);
+            target.CurrentPet.transform.position = target.transform.position;
+            target.CurrentPet.Source = target;
+            target.CurrentPet.Visible = target.Visible;
+            PlayerControl.SetPlayerMaterialColors(colorId, target.CurrentPet.rend);
+        }
+    }
+}
